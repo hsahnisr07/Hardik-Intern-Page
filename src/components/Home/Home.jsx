13deb07ffcb2024940df20b46cbfc686064fa5cc
IshitaGarg-01 <79802180+IshitaGarg-01@users.noreply.github.com>
2022-06-09 21:45:43 +0530

    added carousel

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from "react";
 import "./Home.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import CarouselContainer from "./Carousel_Home/CarouselContainer";
 
 export default function Home() {
   return (
-    <div className='home_body'>this is home</div>
-  )
+    <div className="home_body">
+      <CarouselContainer />
+    </div>
+  );
 }
