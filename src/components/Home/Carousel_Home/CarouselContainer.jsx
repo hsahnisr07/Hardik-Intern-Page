3c7818989353657141412459f7a5a500a0e18db3
PiyushChittora <piyushchittora35246@gmail.com>
2022-06-11 01:12:43 +0530

    chenges

--- src/components/Home/Carousel_Home/CarouselContainer.jsx
+++ src/components/Home/Carousel_Home/CarouselContainer.jsx
@@ -4,78 +4,25 @@ import { Carousel } from "react-bootstrap";
 import { Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import slide1 from "../../../Assets/img_home/img1.png";
-import slide2 from "../../../Assets/img_home/img2.png";
-import slide3 from "../../../Assets/img_home/img3.png";
-import slide4 from "../../../Assets/img_home/img4.png";
+import HomeCarouselContent from "./Carousel_data"
 
 function CarouselContainer() {
+  
   return (
     <Carousel fade={true}>
-      <Carousel.Item interval={2000}>
-        <img className="d-block w-100" src={slide1} alt="First slide" />
-        <Carousel.Caption>
-          <h2>Alumni Talk #1</h2>
-          <p>
-            A common place for all the movie freaks to discuss their hearts out!
-            To explore the intricacies of the the cinematic world. <br></br>A
-            film is – or should be – more like music than like fiction. It
-            should be a progression of moods and feelings. The theme, what’s
-            behind the emotion, the meaning, all that comes later?
-          </p>
-          <Button color="danger">Watch Now</Button>
-        </Carousel.Caption>
-        {/* <p class="mobile_caption">
-          A common place for all the movie freaks to discuss their hearts out!
-          To explore the intricacies of the the cinematic world. <br></br>A film
-          is – or should be – more like music than like fiction. It should be a
-          progression of moods and feelings. The theme, what’s behind the
-          emotion, the meaning, all that comes later?
-        </p> */}
-      </Carousel.Item>
-
-      <Carousel.Item interval={2000}>
-        <img className="d-block w-100" src={slide2} alt="Second slide" />
-        <Carousel.Caption>
-          <h2>Alumni Talk #2</h2>
-          <p>
-            A common place for all the movie freaks to discuss their hearts out!
-            To explore the intricacies of the the cinematic world. <br></br>A
-            film is – or should be – more like music than like fiction. It
-            should be a progression of moods and feelings. The theme, what’s
-            behind the emotion, the meaning, all that comes later?
-          </p>
-          <Button color="danger">Watch Now</Button>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item interval={2000}>
-        <img className="d-block w-100" src={slide3} alt="Third slide" />
-        <Carousel.Caption>
-          <h2>Alumni Talk #3</h2>
-          <p>
-            A common place for all the movie freaks to discuss their hearts out!
-            To explore the intricacies of the the cinematic world. <br></br>A
-            film is – or should be – more like music than like fiction. It
-            should be a progression of moods and feelings. The theme, what’s
-            behind the emotion, the meaning, all that comes later?
-          </p>
-          <Button color="danger">Watch Now</Button>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item interval={2000}>
-        <img className="d-block w-100" src={slide4} alt="Third slide" />
-        <Carousel.Caption>
-          <h2>Alumni Talk #4</h2>
-          <p>
-            A common place for all the movie freaks to discuss their hearts out!
-            To explore the intricacies of the the cinematic world. <br></br>A
-            film is – or should be – more like music than like fiction. It
-            should be a progression of moods and feelings. The theme, what’s
-            behind the emotion, the meaning, all that comes later?
-          </p>
-          <Button color="damger">Watch Now</Button>
-        </Carousel.Caption>
-      </Carousel.Item>
+      {HomeCarouselContent.map((slide, idx) => {
+        return (
+          <Carousel.Item interval={2000}>
+            <img className="d-block w-75 m-auto" src={slide.img} alt="First slide" />
+            <Carousel.Caption>
+              <h2>{slide.heading}</h2>
+              <p>{slide.description}</p>
+              <Button color="danger">
+                <a href={slide.link}>Watch Now</a></Button>
+            </Carousel.Caption>
+          </Carousel.Item>
+        )
+      })}
     </Carousel>
   );
 }
---
+++ src/components/Home/Carousel_Home/Carousel_data.js
@@ -0,0 +1,49 @@
+import slide1 from "../../../Assets/img_home/img1.png";
+import slide2 from "../../../Assets/img_home/img2.png";
+import slide3 from "../../../Assets/img_home/img3.png";
+import slide4 from "../../../Assets/img_home/img4.png";
+
+const HomeCarouselContent = [
+    {
+        img: slide1,
+        heading: "Alumni Talk #1",
+        description: `A common place for all the movie freaks to discuss their hearts out!
+                To explore the intricacies of the the cinematic world. <br></br>A
+                film is – or should be – more like music than like fiction. It
+                should be a progression of moods and feelings. The theme, what’s
+                behind the emotion, the meaning, all that comes later?`,
+        link: "https://youtu.be/1bmihxBedxo",
+    },
+    {
+        img: slide2,
+        heading: "Alumni Talk #2",
+        description: `A common place for all the movie freaks to discuss their hearts out!
+                To explore the intricacies of the the cinematic world. <br></br>A
+                film is – or should be – more like music than like fiction. It
+                should be a progression of moods and feelings. The theme, what’s
+                behind the emotion, the meaning, all that comes later?`,
+        link: "https://youtu.be/1bmihxBedxo",
+    },
+    {
+        img: slide3,
+        heading: "Alumni Talk #3",
+        description: `A common place for all the movie freaks to discuss their hearts out!
+                To explore the intricacies of the the cinematic world. <br></br>A
+                film is – or should be – more like music than like fiction. It
+                should be a progression of moods and feelings. The theme, what’s
+                behind the emotion, the meaning, all that comes later?`,
+        link: "https://youtu.be/1bmihxBedxo",
+    },
+    {
+        img: slide4,
+        heading: "Alumni Talk #4",
+        description: `A common place for all the movie freaks to discuss their hearts out!
+                To explore the intricacies of the the cinematic world. <br></br>A
+                film is – or should be – more like music than like fiction. It
+                should be a progression of moods and feelings. The theme, what’s
+                behind the emotion, the meaning, all that comes later?`,
+        link: "https://youtu.be/1bmihxBedxo",
+    }
+]
+
+export default HomeCarouselContent;
