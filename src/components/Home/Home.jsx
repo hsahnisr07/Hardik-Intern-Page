b17d76ff5a80cd32b29de46346e78a8b57cb2e57
PiyushChittora <piyushchittora35246@gmail.com>
2022-06-15 22:35:22 +0530

    updates added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Home.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import LandingSection  from "./LandingSection/LandingSection";
 import CarouselContainer from "./Carousel_Home/CarouselContainer";
 import Updates from "./Updates/Updates";
 import Home_Carousel from "./Carousel_Home/Home_Carousel";
@@ -8,9 +9,10 @@ import Home_Carousel from "./Carousel_Home/Home_Carousel";
 export default function Home() {
   return (
     <div className="home_body">
+      <LandingSection />
       {/* <CarouselContainer /> */}
-      <Updates/>
-      <Home_Carousel/>
+      {/* <Updates/> */}
+      {/* <Home_Carousel/> */}
     </div>
   );
 }
---
+++ src/components/Home/LandingSection/LandingSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function LandingSection() {
+  return (
+    <div className="LandingSection_container">
+          <h1>EESS, IIT Roorkee</h1>
+          <h3>BE ELECTRIFIED!</h3>
+          <p>{`We are a group of students representing the Electrical family of IIT Roorkee who aim to promote interaction between all the students and faculty. We strive to provide any kind of assistance that enhances the quality of R students’ journey at IIT Roorkee…`}</p>
+    </div>
+  )
+}
