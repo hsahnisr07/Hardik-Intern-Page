81f9a210ffd5dd4253cbbd7dac8d4268f984f0a2
Rahul Modi <rahul_km@ee.iitr.ac.in>
2022-06-21 15:36:43 +0530

    About Redirect Fix

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,22 +1,21 @@
-import React from 'react'
-import { Routes, Route} from "react-router-dom";
-import './About.css'
-import Gallery from './Gallery/Gallery';
-import OurTeam from './OurTeam/OurTeam'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import "./About.css";
+import Gallery from "./Gallery/Gallery";
+import OurTeam from "./OurTeam/OurTeam";
 
-import WhoWeAre from './WhoWeAre/WhoWeAre'
+import WhoWeAre from "./WhoWeAre/WhoWeAre";
 
 export default function About() {
-  
   return (
-    <div className='about_body'>
-      <WhoWeAre/>
+    <div className="about_body">
+      <WhoWeAre />
       <div className="dep_image"></div>
       <Gallery />
       <Routes>
-          <Route exact path={"/"} element={<OurTeam/>} />
-          <Route path={"/:vertical"} element={<OurTeam/>} />
+        <Route path={"/"} element={<OurTeam />} />
+        <Route path={"/:vertical"} element={<OurTeam />} />
       </Routes>
     </div>
-  )
+  );
 }
